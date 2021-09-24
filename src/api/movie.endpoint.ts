fdf4d306ad26c2dca966b07b944dc8835ad4d91d
Fikir Yibeltal <Fyibeltal@excellerentsolutions.com>
2021-09-24 09:07:56 +0300

    configure environment 2

--- src/api/movie.endpoint.ts
+++ src/api/movie.endpoint.ts
@@ -1,4 +1,4 @@
-import { environment } from "../environments/environment.dev";
+import { environment } from "./../environments/environment.dev";
 
 export const MovieEndpoint = {
   getTopRatedMovies: `https://api.themoviedb.org/3/movie/top_rated?api_key=${environment.TMDB_API_KEY}`,
